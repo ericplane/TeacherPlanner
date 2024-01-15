384a8cd78c1c9a436de89f7ec5f81c3d60be67e1
ericplane <58905771+ericplane@users.noreply.github.com>
2024-01-15 23:19:43 +0100

    Data Update

--- src/Data/DataManage.java
+++ src/Data/DataManage.java
@@ -3,28 +3,41 @@ package Data;
 import Classes.SchoolClass;
 
 import java.io.*;
+import java.net.URISyntaxException;
 
 public class DataManage {
     public void saveClassData(SchoolClass[] classes) {
-        try (ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream("data.dat"))) {
+        try {
+            String jarPath = new File(DataManage.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+            File dataFile = new File(new File(jarPath).getParent(), "data.dat");
+            ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream(dataFile));
             stream.writeObject(classes);
             System.out.println("Class Data saved successfully");
-        } catch (IOException ignored) {}
+            stream.close();
+        } catch (IOException | URISyntaxException ignored) {}
     }
 
     public SchoolClass[] loadClassData() {
-        try (ObjectInputStream stream = new ObjectInputStream(new FileInputStream("data.dat"))) {
+        try {
+            String jarPath = new File(DataManage.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+            File dataFile = new File(new File(jarPath).getParent(), "data.dat");
+            ObjectInputStream stream = new ObjectInputStream(new FileInputStream(dataFile));
             SchoolClass[] classes = (SchoolClass[]) stream.readObject();
             System.out.println("Class Data loaded successfully");
+            stream.close();
             return classes;
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException | URISyntaxException e) {
             return null;
         }
     }
 
     public static void createNewDataFile() {
-        try (ObjectOutputStream ignored = new ObjectOutputStream(new FileOutputStream("data.dat"))) {
+        try {
+            String jarPath = new File(DataManage.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+            File dataFile = new File(new File(jarPath).getParent(), "data.dat");
+            ObjectOutputStream ignored = new ObjectOutputStream(new FileOutputStream(dataFile));
             System.out.println("Data file created successfully");
-        } catch (IOException ignored) {}
+            ignored.close();
+        } catch (IOException | URISyntaxException ignored) {}
     }
 }
